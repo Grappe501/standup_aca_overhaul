2d23555585edf802265b033abd4cb94eebbf8824
Grappe501 <grappe4arkansas@gmail.com>
2026-01-02 11:17:54 -0600

    Static build hardening: route normalization + build-safe pages

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,12 +1,14 @@
 
 export default new Map([
 ["src/content/docs/congress.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fcongress.mdx&astroContentModuleFlag=true")],
+["src/content/docs/proposal.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fproposal.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/loopholes.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Floopholes.mdx&astroContentModuleFlag=true")],
-["src/content/docs/rural.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Frural.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Findex.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/start-here.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fstart-here.mdx&astroContentModuleFlag=true")],
-["src/content/docs/the-backbone.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fthe-backbone.mdx&astroContentModuleFlag=true")],
-["src/content/docs/proposal.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fproposal.mdx&astroContentModuleFlag=true")],
+["src/content/docs/rural.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Frural.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/the-problem.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fthe-problem.mdx&astroContentModuleFlag=true")],
-["src/content/docs/congress/legislative.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fcongress%2Flegislative.mdx&astroContentModuleFlag=true")]]);
+["src/content/docs/the-backbone.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fthe-backbone.mdx&astroContentModuleFlag=true")],
+["src/content/docs/congress/legislative.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fcongress%2Flegislative.mdx&astroContentModuleFlag=true")],
+["src/content/docs/docs_migrated/start-here.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fdocs_migrated%2Fstart-here.mdx&astroContentModuleFlag=true")],
+["src/content/docs/docs_migrated/why-healthcare-costs-so-much.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fdocs_migrated%2Fwhy-healthcare-costs-so-much.mdx&astroContentModuleFlag=true")]]);
 		
